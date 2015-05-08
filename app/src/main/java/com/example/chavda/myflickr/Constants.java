443a343efb4bff903e292af567ae76ae852e759e
pchavda <chavda.parth@yahoo.com>
2015-05-08 11:17:56 -0700

    api changes

--- app/src/main/java/com/example/chavda/myflickr/Constants.java
+++ app/src/main/java/com/example/chavda/myflickr/Constants.java
@@ -3,14 +3,14 @@ package com.example.chavda.myflickr;
 public class Constants {
     public static final int NUM_PHOTOS = 10;
     public static final String TAG = "my code";
-    public static final String API_KEY = "8fd9185a9077637f9b6375d3e1394c7c";
-    public static final String SECRET = "b1c8f0151a6c428c";
+    public static final String API_KEY = "4ffd68f3c8821fd6705a7dd873a031db";
+    public static final String SECRET = "413b91c462340e32";
 //    public static final String LATITUDE = "34.0672280";
 //    public static final String LONGITUDE = "-118.1667410";
     public static final String RADIUS = "1";
 
-    public static final String LATITUDE = "40.7127837";
-    public static final String LONGITUDE = "-74.00594130000002";
+    public static final String LATITUDE = "34.0522340";
+    public static final String LONGITUDE = "-118.2436850";
 
 
 
